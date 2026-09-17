d6935976a8d94fa834435d621c5914323e28a73f
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-09-17 13:47:29 -0400

    commit

--- const.js
+++ const.js
@@ -1,4 +1,6 @@
 function add(x,y) {
-    return x + y;
+    return x + y; //allows the result to be used by global scope
 }
 console.log(add(5, 10));
+//'==' converts to same type, '===' does not convert types
+//Ex. 3=='3' is true, 3==='3' is false
